32618e1a9968102a1184fc81ad69a1a073d0f39e
kbriannicolas <karaim.brian@gmail.com>
2022-11-29 11:38:11 -0300

    vuelta al commit anterior

--- clse13.js
+++ clse13.js
@@ -1,6 +1,1 @@
 console.log ("clase 13");
-//clase 13
-// console.log
-console.log ("clase 13");
-console.log("----------------------------------------------------------");
-console.log ("hola clase")
